02b25798067f93afc1d5269664a4fadaa174b58f
Pavel0514 <paxa.russsckih@yandex.ru>
2023-09-21 16:38:25 +0500

    add file

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
-def calculate_ticket_cost():
-    ticket_count = int(input("Введите количество билетов: "))
-    total_cost = 0
+def binary_search(arr, target):
+    left = 0
+    right = len(arr) - 1
 
-    for _ in range(ticket_count):
-        age = int(input("Введите возраст посетителя: "))
-
-        if age < 18:
-            ticket_cost = 0
-        elif 18 <= age < 25:
-            ticket_cost = 990
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
         else:
-            ticket_cost = 1390
+            right = mid - 1
 
-        total_cost += ticket_cost
+    return left
 
-    if ticket_count > 3:
-        total_cost *= 0.9
+def sort_list(arr):
+    return sorted(arr)
 
-    return total_cost
+input_sequence = input("Введите последовательность чисел через пробел: ")
+target_number = input("Введите любое число: ")
 
+try:
+    input_list = list(map(int, input_sequence.split()))
+    target = int(target_number)
 
-total_cost = calculate_ticket_cost()
-print("Сумма к оплате:", total_cost, "руб.")
+    sorted_list = sort_list(input_list)
+    position = binary_search(sorted_list, target)
+
+    if position >= len(sorted_list):
+        print("Число не может быть вставлено в указанную позицию")
+    else:
+        sorted_list.insert(position, target)
+        print(sorted_list)
+except ValueError:
+    print("Ошибка ввода данных. Пожалуйста, введите числа, разделенные пробелом.")
