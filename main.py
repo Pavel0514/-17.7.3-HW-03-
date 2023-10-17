c68c137dd8c755375dde0cbd5af2b044e3426ca3
Pavel0514 <paxa.russsckih@yandex.ru>
2023-10-17 16:09:54 +0500

    add file

--- main.py
+++ main.py
@@ -1,35 +1,82 @@
-def binary_search(arr, target):
-    left = 0
-    right = len(arr) - 1
+import telebot
+import requests
+import json
 
-    while left <= right:
-        mid = (left + right) // 2
-        if arr[mid] == target:
-            return mid
-        elif arr[mid] < target:
-            left = mid + 1
-        else:
-            right = mid - 1
 
-    return left
+TOKEN = '6524965223:AAGJ5frWTRdV00iTpdzpW-YT1e78eisYY0o'
 
-def sort_list(arr):
-    return sorted(arr)
 
-input_sequence = input("Введите последовательность чисел через пробел: ")
-target_number = input("Введите любое число: ")
+bot = telebot.TeleBot(TOKEN)
 
-try:
-    input_list = list(map(int, input_sequence.split()))
-    target = int(target_number)
+keys = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар': 'USD',
+    'рубль': 'RUB',
+    'евро': "EUR"
+}
 
-    sorted_list = sort_list(input_list)
-    position = binary_search(sorted_list, target)
 
-    if position >= len(sorted_list):
-        print("Число не может быть вставлено в указанную позицию")
+class ConvertionException(Exception):
+    pass
+
+class CryptoConverter:
+    @staticmethod
+    def convert(quote: str, base: str, amount: str):
+        if quote == base:
+            raise ConvertionException(f'Невозможно перевести одинаковые валюты {base}.')
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise ConvertionException(f'Не удалось обработать валюту {quote}')
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise ConvertionException(f'Не удалось обработать валюту {base}')
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise ConvertionException(f'Не удалось обработать количество {amount}')
+
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
+        total_base = json.loads(r.content)[keys[base]]
+
+        return total_base
+
+@bot.message_handler(commands=['start', 'help', 'старт'])
+def main(message: telebot.types.Message):
+    text = 'Чтобы начать работу введите комманду боту в следующем формате:\n<имя валюты> \
+<в какую валюту перевести> \
+<количество переводимой валюты>\nУвидить список всех доступных валют: /values, /валюта'
+    bot.reply_to(message, text)
+
+@bot.message_handler(commands=['values', 'валюта'])
+def values(message: telebot.types.Message):
+    text = 'Доступные валюты:'
+    for key in keys.keys():
+        text = '\n'.join((text, key,))
+    bot.reply_to(message, text)
+
+@bot.message_handler(content_types=['text', ])
+def convert(message: telebot.types.Message):
+    try:
+        values = message.text.split(' ')
+
+        if len(values) != 3:
+            raise ConvertionException('Неверная команда.')
+
+        quote, base, amount = values
+        total_base = CryptoConverter.convert(quote, base, amount)
+    except ConvertionException as e:
+        bot.reply_to(message, f'Ошибка пользователя.\n{e}')
+    except Exception as e:
+        bot.reply_to(message, f'Не удалось обработать команду\n{e}')
     else:
-        sorted_list.insert(position, target)
-        print(sorted_list)
-except ValueError:
-    print("Ошибка ввода данных. Пожалуйста, введите числа, разделенные пробелом.")
+        text = f'Цена {amount} {quote} в {base} - {total_base}'
+        bot.send_message(message.chat.id, text)
+
+
+bot.polling()
